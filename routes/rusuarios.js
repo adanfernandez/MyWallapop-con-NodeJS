41884d4fc807de2eb3cf298282ff19b0cad3eb6d
adanvetusta <adanvetusta@gmail.com>
2019-04-14 17:51:34 +0200

    Loggeo de usuarios y comprobación de registro de usuarios.

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -4,24 +4,51 @@ module.exports = function(app, swig, gestorBDUsuarios) {
         var respuesta = swig.renderFile('views/bregistro.html', {});
         res.send(respuesta);
     });
-
+    app.get("/identificarse", function(req, res) {
+        var respuesta = swig.renderFile('views/bidentificacion.html', {});
+        res.send(respuesta);
+    });
     app.post('/usuario', function(req, res) {
         var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
         var usuario = {
             email : req.body.email,
             password : seguro,
-            admin : false
+            admin : false,
+            dinero : parseFloat("100")
         };
-        gestorBDUsuarios.insertarUsuario(usuario, function(id) {
-            if (id == null){
-                res.send("Error al insertar ");
-            } else {
-                res.send('Usuario Insertado ' + id);
+        var criterio = {
+            email : req.body.email
+        };
+        gestorBDUsuarios.obtenerUsuarios(criterio, function (users) {
+            if(users.length > 0) {
+                res.redirect("/registrarse?mensaje=Usuario ya existente" + "&tipoMensaje=alert-danger");
+            }
+            else
+            {
+                gestorBDUsuarios.insertarUsuario(usuario, function(id) {
+                    if (id == null){
+                        res.redirect("/registrarse?mensaje=Error al registrar usuario")
+                    } else {
+                        res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+                    }
+                });
             }
         });
-
     });
-
-
+    app.post("/identificarse", function(req, res) {
+        var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        var criterio = {
+            email : req.body.email,
+            password : seguro
+        }
+        gestorBDUsuarios.obtenerUsuarios(criterio, function(usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                res.send("No identificado: ");
+            } else {
+                res.send("identificado");
+            }
+        });
+    });
 };

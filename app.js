73248ecd4738adb9663d8742b697eb0432ce357d
adanvetusta <adanvetusta@gmail.com>
2019-04-14 14:00:22 +0200

    Adicción de productos propios.

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // Módulos
 var express = require('express');
 var app = express();
+var fileUpload = require('express-fileupload');
+app.use(fileUpload());
 var mongo = require('mongodb');
 var swig = require('swig');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app, mongo);
+var gestorBDProductos = require("./modules/gestorBDProductos.js");
+var gestorBDUsuarios = require("./modules/gestorBDUsuarios.js");
+gestorBDProductos.init(app, mongo);
+gestorBDUsuarios.init(app, mongo);
 app.use(express.static('public'));
 // Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi123456789@sdi-actividad2-204-shard-00-00-pv4pc.mongodb.net:27017,sdi-actividad2-204-shard-00-01-pv4pc.mongodb.net:27017,sdi-actividad2-204-shard-00-02-pv4pc.mongodb.net:27017/test?ssl=true&replicaSet=sdi-actividad2-204-shard-0&authSource=admin&retryWrites=true');
 //Rutas/controladores por lógica
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig, gestorBD);
-require("./routes/rproductos.js")(app, swig, gestorBD);
+require("./routes/rusuarios.js")(app, swig, gestorBDUsuarios);
+require("./routes/rproductos.js")(app, swig, gestorBDProductos);
 // lanzar el servidor
 app.listen(app.get('port'), function() {
     console.log("Servidor activo");
--- routes/rproductos.js
+++ routes/rproductos.js
@@ -1,46 +1,68 @@
-module.exports = function(app, swig, gestorBD) {
+module.exports = function(app, swig, gestorBDProductos) {
     app.post("/producto", function(req, res) {
-
+        var date = new Date();
         var producto = {
             nombre : req.body.nombre,
             descripcion : req.body.descripcion,
-            precio : req.body.precio
+            precio : req.body.precio,
+            fecha: date.getUTCDate() + '/' + (date.getUTCMonth() + 1) + '/' + date.getFullYear(),
+            propietario : '',
+            comprador : ""
         };
-        // Conectarse
-        gestorBD.insertarProducto(producto, function(id){
+        gestorBDProductos.insertarProducto(producto, function(id){
             if (id == null) {
                 res.send("Error al insertar producto");
             } else {
-                res.send("Producto agregado: " + id);
+                if (req.files.imagen != null) {
+                    var imagen = req.files.imagen;
+                    imagen.mv('public/portadas/' + id + '.png', function(err) {
+                        if (err) {
+                            res.send("Error al subir la imagen");
+                        } else {
+                            res.send("Agregado id: " + id);
+                        }
+                    });
+                }
             }
         });
 
     });
-
-
     app.get('/productos/agregar', function (req, res) {
         var respuesta = swig.renderFile('views/bagregar.html', {
-
-        });
-        res.send(respuesta);
-    })
-
-
-    app.get("/productos", function(req, res) {
-        var productos = [ {
-            "nombre" : "Blank space",
-            "precio" : "1.2"
-        }, {
-            "nombre" : "See you again",
-            "precio" : "1.3"
-        }, {
-            "nombre" : "Uptown Funk",
-            "precio" : "1.1"
-        } ];
-        var respuesta = swig.renderFile('views/btienda.html', {
-            vendedor : 'Tienda de canciones',
-            productos : productos
         });
         res.send(respuesta);
     });
+
+    app.get("/tienda", function(req, res) {
+        var criterio  = {};
+        if( req.query.busqueda != null ){
+            criterio = { "nombre" :  {$regex : ".*"+req.query.busqueda +".*", $options: 'i'} };
+        }
+        gestorBDProductos.obtenerProductos( criterio, function(productos) {
+            if (productos == null) {
+                res.send("Error al listar productos");
+            } else {
+                var respuesta = swig.renderFile('views/btienda.html',
+                    {
+                        productos : productos
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    app.get('/producto/:id', function (req, res) {
+        var criterio = { "_id" : gestorBDProductos.mongo.ObjectID(req.params.id)  };
+        gestorBDProductos.obtenerProductos(criterio,function(productos){
+            if ( productos == null ){
+                res.send(respuesta);
+            } else {
+                var respuesta = swig.renderFile('views/bproducto.html',
+                    {
+                        producto : productos[0]
+                    });
+                res.send(respuesta);
+            }
+        });
+    })
 };
